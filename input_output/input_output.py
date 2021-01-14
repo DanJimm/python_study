69ee593b8e4e7068f06c3c13205aac4969d41b96
DanJimm <309802665@qq.com>
2021-01-14 11:03:19 +0800

    asyncio_study

---
+++ input_output/input_output.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Time        : 2020/12/16
+@Author      : jim
+@File        : input_output
+@Description : 
+"""
+
+import re
+
+def parse(text):
+    
+
+with open('in.txt', 'r') as fin:
+    pass
